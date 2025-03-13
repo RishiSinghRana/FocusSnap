ca2255e14bdd7bced8781d5d1d7f347f71e1b63a
RishiSinghRana <rishisinghrana08@gmail.com>
2025-03-13 16:19:14 +0530

    Added Screens

---
+++ app/(tabs)/TaskHistory.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TaskHistory = () => {
+  return (
+    <View>
+      <Text>TaskHistory</Text>
+    </View>
+  )
+}
+
+export default TaskHistory
