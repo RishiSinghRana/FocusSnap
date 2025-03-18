427c55b57e79907037cf6f860e0fe47e356c0589
RishiSinghRana <rishisinghrana08@gmail.com>
2025-03-19 00:00:17 +0530

    Layout to hide edit task header

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,6 +3,7 @@ import './global.css'
 export default function RootLayout() {
   return <Stack>
     <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-    <Stack.Screen name="AddTask" options={{ title: "Add Task", headerShown: false}} />
+    <Stack.Screen name="components/AddTask" options={{ title: "Add Task", headerShown: false}} />
+    <Stack.Screen name="components/EditTask" options={{ title: "Add Task", headerShown: false}} />
   </Stack>;
 }
