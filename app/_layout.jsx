20170055823889633b4cd0bd0247760a60d4a545
RishiSinghRana <rishisinghrana08@gmail.com>
2025-04-14 04:02:22 +0530

    Removed Header for Edit Profile

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,7 +4,8 @@ export default function RootLayout() {
   return <Stack>
     <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
     <Stack.Screen name="components/AddTask" options={{ title: "Add Task", headerShown: false}} />
-    <Stack.Screen name="components/EditTask" options={{ title: "Add Task", headerShown: false}} />
-    <Stack.Screen name="components/TaskDetail" options={{ title: "Add Task", headerShown: false}} />
+    <Stack.Screen name="components/EditTask" options={{ title: "Edit Task", headerShown: false}} />
+    <Stack.Screen name="EditProfile" options={{ title: "Edit Profile", headerShown: false}} />
+    <Stack.Screen name="components/TaskDetail" options={{ title: "Task Detail", headerShown: false}} />
   </Stack>;
 }
